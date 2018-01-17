08e7f511df5353642483f6cc613b6f859edc99a7
hdd2k <hank.lee@meshkorea.net>
2018-01-17 11:25:50 +0900

    tried seeding... but going on to Controllers now since no need to test for right now

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+//        Note : Used for random seeding logic
+//        App\Product::create([
+//            'name' => sprintf('%s', str_random())
+//        ]);
+
+        // Creates 5 new entries & saves
+    }
+}
---
+++ database/seeds/ReviewsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ReviewsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//        Note : Used for random seeding logic
+//        App\Review::create([
+//            'content' => sprintf('%s', str_random())
+//        ]);
+
+    }
+}
