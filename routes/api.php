86eae8968eabb093b9cfcc81262f127418079664
hdd2k <hank.lee@meshkorea.net>
2018-01-19 12:10:20 +0900

    Routes now have auth.basic middleware ... assigned by Route group

--- routes/api.php
+++ routes/api.php
@@ -14,19 +14,22 @@ use Illuminate\Http\Request;
 */
 
 // Product routes
-Route::get('products', 'ProductsController@listProducts');
-Route::get('products/{productId}', 'ProductsController@getProduct');
-Route::post('products', 'ProductsController@createProduct');
-Route::put('products/{productId}', 'ProductsController@updateProduct');
-Route::delete('products/{productId}', 'ProductsController@deleteProduct');
+Route::group(['middleware' => 'auth.basic'], function() {
+    Route::get('products', 'ProductsController@listProducts');
+    Route::get('products/{productId}', 'ProductsController@getProduct');
+    Route::post('products', 'ProductsController@createProduct');
+    Route::put('products/{productId}', 'ProductsController@updateProduct');
+    Route::delete('products/{productId}', 'ProductsController@deleteProduct');
+});
 
 // Review routes
-Route::get('products/{productId}/reviews', 'ReviewsController@listReviews');
-Route::get('products/{productId}/reviews/{reviewId}', 'ReviewsController@getReview');
-Route::post('products/{productId}/reviews', 'ReviewsController@createReview');
-Route::put('products/{productId}/reviews/{reviewId}', 'ReviewsController@updateReview');
-Route::delete('products/{productId}/reviews/{reviewId}', 'ReviewsController@deleteReview');
-
+Route::group(['middleware' => 'auth.basic'], function() {
+    Route::get('products/{productId}/reviews', 'ReviewsController@listReviews');
+    Route::get('products/{productId}/reviews/{reviewId}', 'ReviewsController@getReview');
+    Route::post('products/{productId}/reviews', 'ReviewsController@createReview');
+    Route::put('products/{productId}/reviews/{reviewId}', 'ReviewsController@updateReview');
+    Route::delete('products/{productId}/reviews/{reviewId}', 'ReviewsController@deleteReview');
+});
 
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
